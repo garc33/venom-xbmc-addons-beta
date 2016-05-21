5483376de8cbf21ea0d624302f271d5a3825ad51
TmpName <gihubtmp@yopmail.com>
2016-05-21 14:40:43 +0200

    Update hoster.py

--- plugin.video.vstream/resources/lib/gui/hoster.py
+++ plugin.video.vstream/resources/lib/gui/hoster.py
@@ -278,8 +278,10 @@ class cHosterGui:
             return cHosterHandler().getHoster('wstream')
         if ('veevr.' in sHosterUrl):
             return cHosterHandler().getHoster('veevr')
-        if ('watchvideo.' in sHosterUrl):
-            return cHosterHandler().getHoster('watchvideo')
+        if ('drive.google.com' in sHosterUrl):
+            return cHosterHandler().getHoster('googledrive')
+        if ('docs.google.com' in sHosterUrl):
+            return cHosterHandler().getHoster('googledrive')             
 
         #Lien telechargeable a convertir en stream
         if ('1fichier' in sHosterUrl):
