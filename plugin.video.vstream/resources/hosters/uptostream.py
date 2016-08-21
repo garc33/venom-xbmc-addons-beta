8b0765fb9b671b4b53b1de51ec38baecacbbc526
TmpName <gihubtmp@yopmail.com>
2016-08-21 17:15:03 +0200

    Update uptostream.py

--- plugin.video.vstream/resources/hosters/uptostream.py
+++ plugin.video.vstream/resources/hosters/uptostream.py
@@ -1,18 +1,14 @@
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
-from resources.lib.config import cConfig
+from resources.lib.gui.gui import cGui
 from resources.hosters.hoster import iHoster
-import re,urllib2
-import xbmcgui,xbmc
-
-from resources.lib.packer import cPacker
+import urllib,xbmcgui
 
 class cHoster(iHoster):
 
     def __init__(self):
-        self.__sDisplayName = 'Up2Stream'
+        self.__sDisplayName = 'Uptostream'
         self.__sFileName = self.__sDisplayName
-        self.__sHD = ''
 
     def getDisplayName(self):
         return  self.__sDisplayName
@@ -27,13 +23,7 @@ class cHoster(iHoster):
         return self.__sFileName
 
     def getPluginIdentifier(self):
-        return 'up2stream'
-
-    def setHD(self, sHD):
-        self.__sHD = ''
-
-    def getHD(self):
-        return self.__sHD
+        return 'uptostream'
 
     def isDownloadable(self):
         return True
@@ -42,50 +32,98 @@ class cHoster(iHoster):
         return True
 
     def getPattern(self):
-        return '';
+        return ''
         
-    def __getIdFromUrl(self, sUrl):
+    def __getIdFromUrl(self):
+        sPattern = "id=([^<]+)"
+        oParser = cParser()
+        aResult = oParser.parse(self.__sUrl, sPattern)
+        if (aResult[0] == True):
+            return aResult[1][0]
+
+        return ''
+        
+    def __modifyUrl(self, sUrl):
+        if (sUrl.startswith('http://')):
+            oRequestHandler = cRequestHandler(sUrl)
+            oRequestHandler.request()
+            sRealUrl = oRequestHandler.getRealUrl()
+            self.__sUrl = sRealUrl
+            return self.__getIdFromUrl()
+
+        return sUrl;
+        
+    def __getKey(self):
+        oRequestHandler = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequestHandler.request()
+        sPattern = 'flashvars.filekey="(.+?)";'
+        oParser = cParser()
+        aResult = oParser.parse(sHtmlContent, sPattern)
+        if (aResult[0] == True):
+            aResult = aResult[1][0].replace('.','%2E')
+            return aResult
+
         return ''
 
     def setUrl(self, sUrl):
         self.__sUrl = str(sUrl)
+        self.__sUrl = self.__sUrl.replace('http://uptostream.com/', '')
+        self.__sUrl = self.__sUrl.replace('https://uptostream.com/', '')
+        self.__sUrl = self.__sUrl.replace('iframe/', '')
+        self.__sUrl = 'http://uptostream.com/iframe/' + str(self.__sUrl)
 
     def checkUrl(self, sUrl):
         return True
 
-    def __getUrl(self, media_id):
-        return
-        
+    def getUrl(self):
+        return self.__sUrl
+
     def getMediaLink(self):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-
-        api_call =''
-
+        cGui().showInfo('Resolve', self.__sDisplayName, 5)
+        
         oRequest = cRequestHandler(self.__sUrl)
         sHtmlContent = oRequest.request()
         
-        #xbmc.log(str(self.__sUrl))
-        
         oParser = cParser()
-        sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\))<\/script>'
-        
+        sPattern =  "<source src='([^<>']+)' type='[^'><]+?' data-res='([0-9]+p)'"
         aResult = oParser.parse(sHtmlContent, sPattern)
+        
+        stream_url = ''
         
         if (aResult[0] == True):
-            sHtmlContent = cPacker().unpack(aResult[1][0])
-        
-        #xbmc.log(str(sHtmlContent))
-        
-        sPattern = '\("src","([^"]+)"\)'
-        aResult = oParser.parse(sHtmlContent, sPattern)
-        if (aResult[0]):
-            api_call = aResult[1][0]
-        
-        #xbmc.log(str(api_call))
-        
-        if (api_call):
-            return True, api_call
+            url=[]
+            qua=[]
             
+            for aEntry in aResult[1]:
+                url.append(aEntry[0])
+                qua.append(aEntry[1])
+                
+            #Si une seule url
+            if len(url) == 1:
+                stream_url = url[0]
+            #si plus de une
+            elif len(url) > 1:
+                #Afichage du tableau
+                dialog2 = xbmcgui.Dialog()
+                ret = dialog2.select('Select Quality',qua)
+                if (ret > -1):
+                    stream_url = url[ret]
+                else:
+                    return False, False
+            else:
+                return False, False
+            
+            stream_url = urllib.unquote(stream_url)
+            
+            if not stream_url.startswith('http'):
+                stream_url = 'http:' + stream_url
+                
+            return True, stream_url
+        else:
+            cGui().showInfo(self.__sDisplayName, 'Fichier introuvable' , 5)
+            return False, False
+        
         return False, False
